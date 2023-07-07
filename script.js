a8818cdaca528fd55d090ac5bf44adf82e936b56
aditi0319 <114223064+aditi0319@users.noreply.github.com>
2023-07-07 16:04:26 +0530

    Files Uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle the navbar menu on mobile
+const navbarToggle = document.getElementById('navbarToggle');
+const navbarMenu = document.getElementById('navbarMenu');
+
+navbarToggle.addEventListener('click', () => {
+  navbarToggle.classList.toggle('active');
+  navbarMenu.classList.toggle('active');
+});
